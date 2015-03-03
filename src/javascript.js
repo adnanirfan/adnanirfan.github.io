6370606f5676d3c8ee148290adadb610fac04ddd
adnanirfan <adnanshurta@gmail.com>
2015-03-03 12:09:19 +0500

    Added anitmation, background 6-7 hours 1 March, 2015

--- src/javascript.js
+++ src/javascript.js
@@ -16,12 +16,15 @@ function submitInfo(form){
     user1 = document.getElementById("player1").value;
     user2 = document.getElementById("player2").value;
     console.log(form + " " +user1 + " " + user2)
-    $('#box1').fadeOut('slow');
+
+//    document.getElementById('#box').className = '';
+//    $('#form-container').fadeOut('slow');
 //    $("#box1").fadeIn("slow");
 //    $("form").toggle();
-    event.preventDefault();
-}
 
+    event.preventDefault();
+//    animate();
+}
 
 function clicked(ob){
     if(ob.innerHTML==0 || ob.innerHTML==1) {
@@ -42,28 +45,67 @@ function clicked(ob){
 
     }
 }
-
+//****************************************************************************
 function check(obj){
     if(i1.innerHTML==i2.innerHTML && i2.innerHTML==i3.innerHTML && i3.innerHTML==i1.innerHTML && i1.innerHTML!="-"){
-        alert("Player " + (user + 1) + " Wins  1");
+        alert("Player " + (user + 1) + " Wins");
     }else if(i1.innerHTML==i5.innerHTML && i5.innerHTML==i9.innerHTML && i9.innerHTML==i1.innerHTML && i1.innerHTML!="-"){
-        alert("Player " + (user + 1) + " Wins  2");
+        alert("Player " + (user + 1) + " Wins");
     }else if(i1.innerHTML==i4.innerHTML && i4.innerHTML==i7.innerHTML && i7.innerHTML==i1.innerHTML && i1.innerHTML!="-"){
-        alert("Player " + (user + 1) + " Wins  3");
+        alert("Player " + (user + 1) + " Wins");
     }
 
     else if(i2.innerHTML==i5.innerHTML && i5.innerHTML==i8.innerHTML && i8.innerHTML==i2.innerHTML && i2.innerHTML!="-"){
-        alert("Player " + (user + 1) + " Wins  4");
+        alert("Player " + (user + 1) + " Wins");
     }
     else if(i3.innerHTML==i6.innerHTML && i6.innerHTML==i9.innerHTML && i9.innerHTML==i3.innerHTML && i3.innerHTML!="-"){
-        alert("Player " + (user + 1) + " Wins  4");
+        alert("Player " + (user + 1) + " Wins");
     }
 
     else if(i4.innerHTML==i5.innerHTML && i5.innerHTML==i6.innerHTML && i6.innerHTML==i4.innerHTML && i4.innerHTML!="-"){
-        alert("Player " + (user + 1) + " Wins  5");
+        alert("Player " + (user + 1) + " Wins");
     }
     else if(i7.innerHTML==i8.innerHTML && i8.innerHTML==i9.innerHTML && i9.innerHTML==i7.innerHTML && i7.innerHTML!="-"){
-        alert("Player " + (user + 1) + " Wins  5");
+        alert("Player " + (user + 1) + " Wins");
     }
 }
 
+//****************************************************************************************************************//
+
+$(function animate() {
+    // run the currently selected effect
+    function runEffect() {
+        //*************EFFECTS*************
+        // blind, bounce, clip, drop, explode, fold, highlight, puff, pulsate, scale, shake, size, slide
+        // get effect type from
+        var selectedEffect = 'slide';    /*$( "#effectTypes" ).val();*/
+
+        // most effect types need no options passed by default
+        var options = {};
+        // some effects have required parameters
+        if ( selectedEffect === "scale" ) {
+            options = { percent: 0 };
+        } else if ( selectedEffect === "size" ) {
+            options = { to: { width: 200, height: 60 } };
+        }
+
+        // run the effect
+        $( ".effect" ).hide( selectedEffect, options, 1500, callback );
+    };
+
+    // callback function to bring a hidden box back
+    function callback() {
+        setTimeout(function() {
+            $( "#box" ).removeAttr( "style" ).hide().fadeIn();
+        }, 100 );
+    };
+
+    // set effect from select menu value
+    $( "#button" ).click(function() {
+        runEffect();
+    });
+})
+//*************************************************************************************************//
+$("#button").click(function(){
+//    $("#box").removeClass("hide");
+});
